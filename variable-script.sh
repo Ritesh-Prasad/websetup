72ac168fe0fa462cb92580f9985819ee7cf2ca88
Ritesh Prasad <152852634+Ritesh-Prasad@users.noreply.github.com>
2026-06-27 14:55:19 +0530

    Updated variable-script.sh

--- variable-script.sh
+++ variable-script.sh
@@ -7,48 +7,48 @@ url='https://www.tooplate.com/zip-templates/2156_graphite_creative.zip'
 art_name=2156_graphite_creative
 svc=nginx
 ##################################################
-echo"############################################"
-echo"updating the OS"
+echo "############################################"
+echo "updating the OS"
 echo
 apt update
-echo"###########################################"
+echo "###########################################"
 echo
 
-echo"############################################"
-echo"Installing Package"
+echo "############################################"
+echo "Installing Package"
 echo
 sudo apt install  $package -y
-echo"###########################################"
+echo "###########################################"
 echo
 
-echo"############################################"
-echo"Downloading ZIP File"
+echo "############################################"
+echo "Downloading ZIP File"
 wget $url
-echo"###########################################"
+echo "###########################################"
 echo
 
-echo"############################################"
-echo"UNZIP File"
+echo "############################################"
+echo "UNZIP File"
 sudo unzip $art_name.zip
-echo"###########################################"
+echo "###########################################"
 echo
 
-echo"############################################"
-echo"Copying UNZIP File"
+echo "############################################"
+echo "Copying UNZIP File"
 cp -r $art_name/* /var/www/html/.
-echo"###########################################"
+echo "###########################################"
 echo
 
-echo"############################################"
-echo"Restarting Services"
+echo "############################################"
+echo "Restarting Services"
 sudo systemctl restart $svc
-echo"###########################################"
+echo "###########################################"
 echo
 
-echo"############################################"
-echo"Enable Services"
+echo "############################################"
+echo "Enable Services"
 sudo systemctl enable $svc
-echo"###########################################"
+echo "###########################################"
 echo
 
 echo
